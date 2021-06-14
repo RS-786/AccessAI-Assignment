d3be7b505cb7fe8ff590b16f81928b7d3aa0d854
Robin <robinrythm123@gmail.com>
2021-06-14 08:02:53 +0530

    Employee model created

--- assignment/Employee/models.py
+++ assignment/Employee/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+GENDER_CHOICES = (
+    ('M','Male'),
+    ('F','Female'),
+    ('O','Other')
+)
+class Employee(models.Model):
+    name = models.TextField(max_length = 64)
+    gender = models.CharField(choices = GENDER_CHOICES, max_length = 1)
+    salary = models.IntegerField()
+    joining_date = models.DateField(auto_now = True)
+    address = models.TextField(max_length = 100)
+
+    def __str__(self):
+        return self.name
